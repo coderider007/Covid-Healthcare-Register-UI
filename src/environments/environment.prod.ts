b4597384592fefd3803ebb08d53e01c522634fde
Arun Kumar <arunkumar7620@gmail.com>
2020-06-06 05:10:45 +0530

    API url updated to https

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  chrbaseuri: 'http://ec2-13-127-77-144.ap-south-1.compute.amazonaws.com/chr/hospital'
+  chrbaseuri: 'https://ec2-13-127-77-144.ap-south-1.compute.amazonaws.com/chr/hospital'
 };
